9db18ff7e2bf68aa3576d1642358447d980a91ee
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2022-08-26 15:12:36 +0530

    Time: 683 ms (11.28%), Space: 85.6 MB (13.85%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& arr) {
-           int n=arr.size();
-        int totSum=0;
-    
-    for(int i=0; i<n;i++){
-        totSum+= arr[i];
-    }
-    
-    if (totSum%2==1) return false;
-    
-    
-        int k = totSum/2;
-        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
-    
-        for(int i=0; i<n; i++){
-            dp[i][0] = true;
-        }
-        
-        if(arr[0]<=k)
-            dp[0][arr[0]] = true;
-        
-        for(int ind = 1; ind<n; ind++){
-            for(int target= 1; target<=k; target++){
-                
-                bool notTaken = dp[ind-1][target];
-        
-                bool taken = false;
-                    if(arr[ind]<=target)
-                        taken = dp[ind-1][target-arr[ind]];
-            
-                dp[ind][target]= notTaken||taken;
-            }
-        }
-        
-        return dp[n-1][k];
-    
+            int n=arr.size();
+         if(n==0) return false;
+          int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+       if (sum % 2 != 0)
+        return false;
+        sum/=2;
+      vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+      for(int i=0;i<n;i++)dp[i][0]=1;
+        if(arr[0]<=sum)
+       dp[0][arr[0]]=1;
+      for(int i=1;i<n;i++){
+          for(int j=1;j<=sum;j++){
+              int nottake=dp[i-1][j];
+              int take=0;
+              if(arr[i]<=j) take=dp[i-1][j-arr[i]];
+              dp[i][j]=take||nottake;
+          }
+      }
+      return dp[n-1][sum];
     }
 };
