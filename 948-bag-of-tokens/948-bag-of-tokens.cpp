01ac3cdb6a59a6ec528aee428b1a1009788471a8
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2022-09-12 19:47:14 +0530

    Time: 11 ms (52.87%), Space: 10.7 MB (27.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1&& k<nums[0]) return 0;
+        sort(nums.begin(),nums.end());
+        int i=0,j=n-1;
+        int count=0;
+        while(i<j){
+            if(nums[i]<=k){
+                count++;
+                k-=nums[i];
+                i++;
+            }
+            else if(count>0) {
+                count--;
+                k+=nums[j];
+                j--;
+            } else{
+                i++;j--;
+            }
+        }
+        if(i==j && nums[i]<=k){
+            count++;
+        }
+        return count;
+    }
+};
