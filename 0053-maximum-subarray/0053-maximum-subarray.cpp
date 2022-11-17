dc8c5005e0e02cb6203a4811addedb3cd2cfb4ff
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2022-11-17 15:19:44 +0530

    Time: 340 ms (22.54%), Space: 67.8 MB (54.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,9 +3,26 @@ public:
     int maxSubArray(vector<int>& nums) {
        int ans=nums[0];
         int temp=nums[0];
+        int end=0;
         for(int i=1;i<nums.size();i++){
             temp=max(nums[i],temp+nums[i]);
+          if(temp>ans){
+              end=i;
+          }
             ans=max(ans,temp);
+        }
+    int start=end;
+        int gobalmax=ans;
+        while(start>=0){
+            gobalmax-=nums[start];
+            if(gobalmax==0){
+                break;
+            }
+            start--;
+        }
+        
+        for(int i=start;i<=end;i++){
+            cout<<nums[i]<<" ";
         }
         return ans;
     }
