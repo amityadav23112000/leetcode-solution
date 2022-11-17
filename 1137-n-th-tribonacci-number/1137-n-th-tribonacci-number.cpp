e81a722de562cd25946692f58693e144409cf50f
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2022-11-17 14:22:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,13 +4,16 @@ public:
         if(n==0)return 0;
         if(n==1)return 1;
         if(n==2)return 1;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int a=0;
+        int b=1;
+        int c=1;
+        int d;
         for( int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+           d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
         }
-        return dp[n];
+        return d;
     }
 };
