fb28164ea742f531af97f7f5e76f1d9b418b5837
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2024-06-23 14:29:06 +0530

    Time: 9 ms (48.52%), Space: 11.9 MB (54.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,8 +5,8 @@ public:
         int n=s.length();
         int m=t.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++)dp[i][0]=i;
+        for(int j=1;j<=m;j++)dp[0][j]=j;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1])
